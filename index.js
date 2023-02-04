4401b0e7b4d0af798afce4e1884d8171ac761ec9
Kyle <kyleoneill26@gmail.com>
2023-02-04 12:52:26 -0600

    Adds myFile.md

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, name1) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase() === name1.toLowerCase() })
+  }
+  function fuzzyMatch(drivers, name1) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().substring(0, name1.length) === name1.toLowerCase()
+    })
+  }
+  function matchName(driver, name2){
+    return driver.filter(function (driver){
+        return driver.name === name2 
+    })
+  }
